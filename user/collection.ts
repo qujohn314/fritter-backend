49e3fa7ee5c5b93e943293262b89337fdedf84fb
Quincy Johnson <35878908+qujohn314@users.noreply.github.com>
2022-10-26 02:36:47 -0400

    critical bug fixed

--- user/collection.ts
+++ user/collection.ts
@@ -21,7 +21,7 @@ class UserCollection {
   static async addOne(username: string, password: string): Promise<HydratedDocument<User>> {
     const dateJoined = new Date();
 
-    const user = new UserModel({username, password, dateJoined});
+    const user = new UserModel({username, password, dateJoined,silentMode:false,silentReaction:false,silentComment:false});
     await user.save(); // Saves user to MongoDB
     return user;
   }
